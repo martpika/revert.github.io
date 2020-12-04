889529ecd915ef3823938d0aa9f2f9b1ef5dc772
pikamart <pamplona.raymart02@gmail.com>
2020-12-04 10:12:20 -0800

    70% complete

--- app/js/script.js
+++ app/js/script.js
@@ -1,18 +1,27 @@
 function mailupmenu(){
   var x = document.getElementById("changer");
+  document.getElementById("letter-changer").className = "mail";
   if (x.className === "mail-container"){
     x.className += " first";
+    document.getElementById("mail-letter").className = "letter";
   }else{
-  x.className = "mail-container";
-}
+    x.className = "mail-container";
+    document.getElementById("letter-changer").className = "mail";
+  }
 }
 
 function mailupmenulast() {
   var x = document.getElementById("changer");
+  document.getElementById("letter-changer").className = "mail";
   if (x.className === "mail-container first"){
     x.className += " last";
-  }else{
-    x.className = "mail-container first";
   }
+  //else{
+  //   x.className = "mail-container first";
+  //   document.getElementById("mail-letter").className += " lela";
+  // }
 }
 
+function letterback() {
+  document.getElementById("letter-changer").className += " last";
+}
